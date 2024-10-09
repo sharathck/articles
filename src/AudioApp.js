e16246f116d0207f57ceed8dc2d5e1453a99e558
Aarush <aarush@gmail.com>
2024-10-09 14:51:34 -0500

    Add new components for GenAI and Audio applications, implement Firebase integration, and update environment variables

---
+++ src/AudioApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AudioApp() {
+  return (
+    <div>
+      <h1>Invlid AudioApp URL</h1>
+    </div>
+  );
+}
+
+export default AudioApp;
---
+++ src/GenAIApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Invlid Gen AI URL</h1>
+    </div>
+  );
+}
+
+export default App;
